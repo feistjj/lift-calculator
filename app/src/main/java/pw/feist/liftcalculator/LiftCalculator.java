2874fe385e486a51aa2e1b9a3c46a66e25217a11
James Feist <james@feist.pw>
2015-04-01 15:23:10 -0700

    that was dumb

--- app/src/main/java/pw/feist/liftcalculator/LiftCalculator.java
+++ app/src/main/java/pw/feist/liftcalculator/LiftCalculator.java
@@ -378,5 +378,28 @@ public class LiftCalculator extends ActionBarActivity {
         }
     }
 
+    public class PlateWatcher implements TextWatcher {
+        private EditText editText;
+        private LiftCalculator liftCalc;
+
+        public PlateWatcher(LiftCalculator l, EditText e) {
+            liftCalc = l;
+            editText = e;
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //pass
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //pass
+        }
+
+        public void afterTextChanged(Editable s) {
+            liftCalc.createPlates();
+        }
+
+    }
 }
+
 
